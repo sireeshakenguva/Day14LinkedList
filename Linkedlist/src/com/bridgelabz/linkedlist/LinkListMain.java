aea6ef1c2f6a93a9f239598feda6e9aaad46d351
sireeshakenguva <sireeshakenguva@gmail.com>
2022-06-02 10:07:42 +0530

    Added inserted the 30 element in between 56 and 70

--- Linkedlist/src/com/bridgelabz/linkedlist/LinkListMain.java
+++ Linkedlist/src/com/bridgelabz/linkedlist/LinkListMain.java
@@ -3,10 +3,10 @@ package com.bridgelabz.linkedlist;
 public class LinkListMain {
     public static void main(String[] args) {
         System.out.println("Welcome To Linked List Program");
-        LinkedList list=new LinkedList();
+        Linkedlist list=new Linkedlist();
         list.add(56);
-        list.add(30);
         list.add(70);
+        list.insert(list.head, 30);
         list.display();
 
     }
