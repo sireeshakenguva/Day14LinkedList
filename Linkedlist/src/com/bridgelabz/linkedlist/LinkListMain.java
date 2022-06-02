0bf64cdd29eff241bffd57fd16445998d5615b2d
sireeshakenguva <sireeshakenguva@gmail.com>
2022-06-02 10:28:08 +0530

    Added 40 after 30

--- Linkedlist/src/com/bridgelabz/linkedlist/LinkListMain.java
+++ Linkedlist/src/com/bridgelabz/linkedlist/LinkListMain.java
@@ -3,17 +3,20 @@ package com.bridgelabz.linkedlist;
 public class LinkListMain {
     public static void main(String[] args) {
         System.out.println("Welcome To Linked List Program");
+        System.out.println("Nodes of simple linked list:");
         Linkedlist list = new Linkedlist();
+        list.add(56);
         list.add(70);
         list.display();
         System.out.println("30 is Inserting between 56 and 70");
         list.insert(list.head, 30);
         list.display();
-        
-        list.pop();
-        list.display();
+        //list.pop();
+        //list.display();
         list.searchNode(30);
         list.display();
-
+        System.out.println("40 is Inserting after 30 ");
+        list.insert(list.head.next, 40);
+        list.display();
     }
 }
