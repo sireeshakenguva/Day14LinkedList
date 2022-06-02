5d16a14dd5c0d3f014a22ea95e034e06c3bc2c24
sireeshakenguva <sireeshakenguva@gmail.com>
2022-06-02 09:46:32 +0530

    Welcome to Linkedlist program

---
+++ Linkedlist/src/com/bridgelabz/linkedlist/Linkedlist.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.linkedlist;
+
+public class Linkedlist {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Linked List Program");
+    }
+}
